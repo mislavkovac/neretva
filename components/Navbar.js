0aacceb57902e9dc4487c080b7ea882255c008da
mislavkovac <mkovac07@fesb.hr>
2022-02-13 14:02:01 +0100

    Fix

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,11 +5,10 @@ import styles from '../styles/Layout.module.css';
 const Navbar = () => {
 	return (
 		<div className="hidden sm:flex sm:flex-row">
-			<Link
-				href="/categories"
-				className="order-1 no-underline text-[#ffffff] px-[1.5rem] font-semibold transition duration-300 hover:text-[#314455] rounded-full hover:bg-[#C96567] h-[50px] flex text-center justify-center items-center"
-			>
-				Categories
+			<Link href="/categories" passHref>
+				<div className="order-1 no-underline text-[#ffffff] px-[1.5rem] font-semibold transition duration-300 hover:text-[#314455] rounded-full hover:bg-[#C96567] h-[50px] flex text-center justify-center items-center">
+					Categories
+				</div>
 			</Link>
 			<Link
 				href="/discounts"
