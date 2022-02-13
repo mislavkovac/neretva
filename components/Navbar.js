d6b4cbb00fd53f0f17f58973c886031d7638b50a
mislavkovac <mkovac07@fesb.hr>
2022-02-13 14:06:37 +0100

    Fix

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,23 +5,23 @@ import styles from '../styles/Layout.module.css';
 const Navbar = () => {
 	return (
 		<div className="hidden sm:flex sm:flex-row">
-			<Link className="cursor-pointer" href="/categories" passHref>
-				<div className="order-1 no-underline text-[#ffffff] px-[1.5rem] font-semibold transition duration-300 hover:text-[#314455] rounded-full hover:bg-[#C96567] h-[50px] flex text-center justify-center items-center">
+			<Link className="" href="/categories" passHref>
+				<div className="cursor-pointer order-1 no-underline text-[#ffffff] px-[1.5rem] font-semibold transition duration-300 hover:text-[#314455] rounded-full hover:bg-[#C96567] h-[50px] flex text-center justify-center items-center">
 					Categories
 				</div>
 			</Link>
-			<Link className="cursor-pointer" href="/discounts" passHref>
-				<div className="order-1 no-underline text-[#ffffff] px-[1.5rem] font-semibold transition duration-300 hover:text-[#314455] rounded-full hover:bg-[#C96567] h-[50px] flex text-center justify-center items-center">
+			<Link className="" href="/discounts" passHref>
+				<div className="cursor-pointer order-1 no-underline text-[#ffffff] px-[1.5rem] font-semibold transition duration-300 hover:text-[#314455] rounded-full hover:bg-[#C96567] h-[50px] flex text-center justify-center items-center">
 					Discounts
 				</div>
 			</Link>
-			<Link className="cursor-pointer" href="/reviews" passHref>
-				<div className="order-1 no-underline text-[#ffffff] px-[1.5rem] font-semibold transition duration-300 hover:text-[#314455] rounded-full hover:bg-[#C96567] h-[50px] flex text-center justify-center items-center">
+			<Link className="" href="/reviews" passHref>
+				<div className="cursor-pointer order-1 no-underline text-[#ffffff] px-[1.5rem] font-semibold transition duration-300 hover:text-[#314455] rounded-full hover:bg-[#C96567] h-[50px] flex text-center justify-center items-center">
 					Reviews
 				</div>
 			</Link>
-			<Link className="cursor-pointer" href="/faq" passHref>
-				<div className="order-1 no-underline text-[#ffffff] px-[1.5rem] font-semibold transition duration-300 hover:text-[#314455] rounded-full hover:bg-[#C96567] h-[50px] flex text-center justify-center items-center">
+			<Link className="" href="/faq" passHref>
+				<div className="cursor-pointer order-1 no-underline text-[#ffffff] px-[1.5rem] font-semibold transition duration-300 hover:text-[#314455] rounded-full hover:bg-[#C96567] h-[50px] flex text-center justify-center items-center">
 					FAQ
 				</div>
 			</Link>
