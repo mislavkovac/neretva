20eb7a42a20214ceb53308633f26edc1955cb85e
mislavkovac <mkovac07@fesb.hr>
2022-02-13 18:41:13 +0100

    Fix

--- components/FaqMobile.js
+++ components/FaqMobile.js
@@ -198,10 +198,11 @@ const faq = () => {
 						onClick={accountFaq}
 						className="cursor-pointer m-2 w-[80%] items-center justify-between flex flex-row content-between rounded overflow-hidden shadow-xl bg-[#ffffff] border-b-[2px] border-[#C96567] transition duration-500 ease-in-out hover:scale-105"
 					>
-						<ManageAccountsIcon
-							style={{ color: '#C96567', width: '6rem', height: '6rem' }}
-							// className="w-[6rem] h-[6rem]"
-						/>
+						<div className="w-[6rem] h-[6rem]">
+							<ManageAccountsIcon
+								style={{ color: '#C96567', width: '6rem', height: '6rem' }}
+							/>
+						</div>
 						<div
 							style={{
 								color: account ? '#314455' : '#ffffff',
